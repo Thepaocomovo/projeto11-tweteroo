72cb071e99642ee5e82258a493520ae0e0893d47
wesley <wesleysantos20012001@gmail.com>
2022-08-29 09:59:24 -0300

    implemantation of /tweets post and /tweets get

--- src/app.js
+++ src/app.js
@@ -3,43 +3,44 @@ import express from "express";
 const server = express();
 server.use(express.json());
 
-const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	  tweet: "eu amo o hub"
-	},
-    {
-		username: "joao",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	  tweet: "eu amo o hub"
-	},
-];
+const tweets = [];
 let user;
 
 server.post('/sign-up', (req, res) => {
-    if(req.body !== undefined){
-        if(req.body.username !== undefined && req.body.avatar !== undefined) {
-            user = req.body
-            return res.send("ok");
-        }
+    if(req.body.username !== undefined && req.body.avatar !== undefined) {
+        user = req.body
+        return res.send("ok");
     }
-    
     res.send("not ok");
   });
 
 
+server.post('/tweets', (req, res) => {
+if(req.body.username !== undefined && req.body.tweet !== undefined) {
+    tweets.push({
+        username: req.body.username,
+        avatar: user.avatar,
+        tweet: req.body.tweet
+    })
+    return res.send("ok");
+}
 
-
+res.send("not ok");
+});
 
 server.get('/tweets', (req, res) => {
-    res.send(tweets);
+    const sendTweets = []
+    for(let i = tweets.length-1; i > tweets.length-11; i--){
+        if(tweets[i]){
+            sendTweets.push(tweets[i])
+        }
+    }
+    sendTweets.reverse()
+    res.send(sendTweets);
   });
 
 
 
 
-
-console.log("it's")
 
 server.listen(5000, ()=>{console.log("listen on 5000")}); 
