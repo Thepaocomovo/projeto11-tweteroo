9ba8d88e41a553e7f3a7ecb89fdfef920832d20c
wesley <wesleysantos20012001@gmail.com>
2022-08-29 11:00:21 -0300

    status code bonus

--- src/app.js
+++ src/app.js
@@ -1,31 +1,51 @@
 import express from "express";
+import cors from "cors";
+
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 const tweets = [];
-let user;
+let users=[];
+
+function isValidHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+  
+    return true;
+  }
 
 server.post('/sign-up', (req, res) => {
     if(req.body.username !== undefined && req.body.avatar !== undefined) {
-        user = req.body
-        return res.send("ok");
+
+        if(isValidHttpUrl(req.body.avatar)){
+            users.push(req.body);
+            return res.send("ok");
+        }
+        res.status(400).send("Insira uma url valida");
     }
-    res.send("not ok");
+    res.status(400).send("Envie todos os campos necessários");
   });
 
 
 server.post('/tweets', (req, res) => {
-if(req.body.username !== undefined && req.body.tweet !== undefined) {
-    tweets.push({
-        username: req.body.username,
-        avatar: user.avatar,
-        tweet: req.body.tweet
-    })
-    return res.send("ok");
-}
+    if(req.body.username !== undefined && req.body.tweet !== undefined) {
+        const currentUser = users.find((value)=> value.username  === req.body.username)
+        tweets.push({
+            username: req.body.username,
+            avatar: currentUser.avatar,
+            tweet: req.body.tweet
+        })
+        return res.send("ok");
+    }
 
-res.send("not ok");
+    res.status(400).send("Envie todos os campos necessários");
 });
 
 server.get('/tweets', (req, res) => {
@@ -35,9 +55,8 @@ server.get('/tweets', (req, res) => {
             sendTweets.push(tweets[i])
         }
     }
-    sendTweets.reverse()
     res.send(sendTweets);
-  });
+});
 
 
 
